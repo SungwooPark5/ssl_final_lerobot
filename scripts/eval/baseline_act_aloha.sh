612085f06d75176ff50a83f19261ac239d722a1d
SWPark <swpark.biz@gmail.com>
2025-11-18 22:24:05 +0900

    Update Dockerfile to use official lerobot docker image

--- scripts/eval/baseline_act_aloha.sh
+++ scripts/eval/baseline_act_aloha.sh
@@ -1,8 +1,14 @@
+# 직접 훈련시킨 ACT baseline 모델에 대한 검증 스크립트
+# dataset: aloha_sim_trasfer_cube_human
+# task: Aloha transfer cube
+
 lerobot-eval \
-    --policy.path=lerobot/act_aloha_sim_transfer_cube_human \
-    --env.type=aloha \
-    --env.task=AlohaTransferCube-v0 \
-    --eval.batch_size=50 \
-    --eval.n_episodes=500 \
-    --policy.use_amp=false \
-    --policy.device=cuda
+  --policy.path=swpark5/baseline_act_aloha_sim_transfer_cube_80k \
+  --policy.push_to_hub=true \
+  --policy.repo_id=swpark5/eval_baseline_act_aloha_sim_transfer_cube_80k \
+  --env.type=aloha \
+  --env.task=AlohaTransferCube-v0  \
+  --eval.batch_size=1 \
+  --eval.n_episodes=1 \
+  --policy.use_amp=false \
+  --policy.device=cuda
